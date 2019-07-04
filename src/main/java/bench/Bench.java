b3e383e0b32b2dc1f7af7d3db37d87d9118eeb3f
Arthur Godet <arth.godet@gmail.com>
2019-07-04 15:25:30 +0200

    add solution file + correct bench.Bench.main()

--- src/main/java/bench/Bench.java
+++ src/main/java/bench/Bench.java
@@ -16,8 +16,6 @@ import java.io.IOException;
 public class Bench {
 
     public static void main(String[] args) throws IOException, ContradictionException {
-        args = new String[]{"5m", "EASY_2000_500"};
-
         if(!"all".equals(args[1])) {
             Instance instance = Factory.fromFile("data/"+args[1]+".json", Instance.class);
             System.out.println(instance.name);
